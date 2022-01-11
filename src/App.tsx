895a8cab9a5ac247ed0c204f9ef4542adb6d0530
nagai hiroto <nagai@cyberbeing.co.jp>
2022-01-11 23:39:10 +0900

    20220111 コミット

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <_Frame>
+      <_Header>
+
+      </_Header>
+      <_Body>
+
+      </_Body>
+      <_Footer>
+
+      </_Footer>
+    </_Frame>
   );
 }
 
 export default App;
+
+const _Frame = styled.div`
+    display: inline-block;
+    width: 100%;
+    height: 100%;
+    background-color: #eba4a4
+`;
+
+const _Header = styled.div`
+display: inline-block;
+    width: 100%;
+    height: 100%;
+    background-color: #fd0707
+`;
+
+const _Body = styled.div`
+display: inline-block;
+    width: 100%;
+    height: 100%;
+    background-color: #fbff00
+`;
+
+const _Footer = styled.div`
+display: inline-block;
+    width: 100%;
+    height: 100%;
+    background-color: #13ff46
+`;
