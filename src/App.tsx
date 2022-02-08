07b591e0caaf874bc8dc768b1736177c43e9c926
nagai hiroto <nagai@cyberbeing.co.jp>
2022-02-09 01:26:44 +0900

    20220209 永井 変更をコミット

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import styled from 'styled-components';
 
 function App() {
+
+  
+
   return (
     <_Frame>
       <_Header>
+        <_Title>
+        
+        </_Title>
+        <_MenuList>
+          <_MenuItem>
 
+          </_MenuItem>
+        </_MenuList>
       </_Header>
       <_Body>
 
@@ -30,7 +40,35 @@ position: fixed;
 display: inline-block;
     width: 100%;
     height: 40px;
-    background-color: #fd0707
+    background-color: #f7f7f7
+`;
+
+const _Title = styled.div`
+position: fixed;
+display: inline-block;
+    width: 200px;
+    height: 40px;
+    font-size: 10px;
+    background-color: #fd07074c
+`;
+
+const _MenuList = styled.div`
+position: fixed;
+display: inline-block;
+    left: 200px;
+    width: calc(100% - 200px);
+    height: 40px;
+    text-align: center;
+    background-color: #0749fd42
+`;
+
+const _MenuItem = styled.div`
+position: fixed;
+display: inline-block;
+    left: 200px;
+    width: calc(100% - 200px);
+    height: 40px;
+    font-size: 10px;
 `;
 
 const _Body = styled.div`
